232938fe9602f44054682b9f300984e1758ed3c3
Jordan Brantner <thejordanbrantner@gmail.com>
2023-08-02 13:21:19 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+const num1 = 1;
+const num2 = 62;
+const num3 = 10;
+let num4 = (5, 10, 20);
+
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % 6;
+let max = Math.max(num4);
